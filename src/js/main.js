3ec1c38327f520ff7d74719d283fe4f330a04d1b
chriszarate <chris@zarate.org>
2016-02-29 10:55:53 -0500

    Move search hints to encapsulated module.

--- src/js/main.js
+++ src/js/main.js
@@ -4,47 +4,6 @@
 
 'use strict';
 
-var $ = require('jquery');
-require('typeahead.js');
+var jQuery = require('jquery');
 
-// Load terms.
-var searchTerms = require('./data/searchTerms.json');
-
-// Toggle order drop-down.
-$('.order-button-link').on('click', function () {
-  $(this).parent().toggleClass('open');
-});
-
-// Add Typeahead to search field.
-function findMatches (q, cb) {
-
-  var matches = [];
-  var substrRegex = new RegExp(q, 'i');
-
-  $.each(searchTerms, function (i, str) {
-    if (substrRegex.test(str)) {
-      matches.push(str);
-    }
-  });
-
-  cb(matches);
-
-}
-
-var options = {
-  hint: true,
-  highlight: true,
-  minLength: 2
-};
-
-var engine = {
-  name: 'terms',
-  source: findMatches
-};
-
-// Bind to search field.
-$('.search-field')
-  .typeahead(options, engine)
-  .on('typeahead:selected', function () {
-    $(this).closest('form').submit();
-  });
+require('./search-hints')(jQuery);
