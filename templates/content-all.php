c1241d8829fad4f4245cffb20f2b3701723593d0
Ryan Williams <ryrowi@gmail.com>
2016-03-04 11:00:14 -0500

    right align "Newer posts" pagination link

--- templates/content-all.php
+++ templates/content-all.php
@@ -57,7 +57,9 @@ else :
 endif;
 ?>
 
-<div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
-<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
+<div class="pagination">
+	<div class="nav-previous"><?php next_posts_link( 'Older posts' ); ?></div>
+	<div class="nav-next"><?php previous_posts_link( 'Newer posts' ); ?></div>
+</div>
 
 </div>
