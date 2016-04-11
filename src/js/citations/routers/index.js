74b22b580e5ccc13d08eb14c04974fb78ee1e227
chriszarate <chris@zarate.org>
2016-04-10 20:11:50 -0400

    Updated works-cited steps.

--- src/js/citations/routers/index.js
+++ src/js/citations/routers/index.js
@@ -6,8 +6,7 @@ module.exports = function (Module, App, Backbone) {
   return Backbone.Marionette.AppRouter.extend({
     appRoutes: {
       'whats-new/': 'showCitationForm',
-      'mla-style-at-a-glance/': 'showCitationForm',
-      'mla-style-at-a-glance-:type/': 'showCitationForm'
+      'works-cited-a-quick-guide-:type/': 'showCitationForm'
     }
   });
 };
--- src/js/citations/templates/citation-layout.tpl
+++ src/js/citations/templates/citation-layout.tpl
@@ -1,6 +1,6 @@
 <div id="citation-form"></div>
 <div class="citation-output">
-  <h4 class="citation-head">Final works-cited-list entry</h4>
+  <h4 class="citation-output-head">Final works-cited-list entry</h4>
   <p id="citation-el"></p>
 </div>
 <div id="citation-lightbox-placeholder" class="lity-hide"></div>
