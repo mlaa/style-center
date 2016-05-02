15c27bdeb265d3c2ceb48d590604193b51be555e
Ryan Williams <ryrowi@gmail.com>
2016-05-02 15:19:12 -0400

    update contact link in footer

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@ namespace MLA\Commons\Theme\MLAStyleCenter;
 			<ul class="nav nav-pills">
 				<li><a href="/about-the-mla-style-center">About the MLA Style Center</a></li>
 				<li><a href="https://www.mla.org/About-Us/Support/Donate-to-the-MLA">Donate</a></li>
-				<li><a href="https://www.mla.org/About-Us/Contact">Contact</a></li>
+				<li><a href="/contact-us">Contact</a></li>
 			</ul>
 
 			<ul class="nav nav-social">
