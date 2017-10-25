96388ac5c96da45d48df18fce7283191ca040dc6
Ryan Williams <rwilliams@mla.org>
2017-10-25 10:27:09 -0400

    only append ellipses to BTS post excerpts

--- functions.php
+++ functions.php
@@ -241,7 +241,7 @@ add_filter( 'tiny_mce_before_init', __NAMESPACE__ . '\sc_mce_before_init' );
  */
 function sc_add_ellipsis_to_behind_the_style_posts( $excerpt ) {
 	$ellipsis = ' . . .';
-	if ( 0 === preg_match( "/$ellipsis$/i", $excerpt ) ) {
+	if ( is_category( 'behind-the-style' ) && 0 === preg_match( "/$ellipsis$/i", $excerpt ) ) {
 		$excerpt .= $ellipsis;
 	}
 	return $excerpt;
