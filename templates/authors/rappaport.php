1a6d1dbdccb3fa97a1e79c5bb1a4892326a23153
Tom Lewek <t.j.lewek@gmail.com>
2017-11-28 17:31:16 -0500

    job title change

--- templates/authors/rappaport.php
+++ templates/authors/rappaport.php
@@ -1,6 +1,6 @@
 <div class="instructor-intro tile instructor-tile instructor-tile-small">
 <div class="tile-link tile-body">
 		<div class="author-photo author-photo-rappaport"></div>
-		<p><strong>Jennifer Rappaport</strong> is an associate editor at the Modern Language Association. She received a BA in English and French from Vassar College and an MA in comparative literature from New York University, where she taught expository writing. Before coming to the MLA, she worked as an editor at a university press and as a freelance copyeditor and translator for commercial and academic publishers.</p>
+		<p><strong>Jennifer Rappaport</strong> is managing editor, MLA style resources, at the Modern Language Association. She received a BA in English and French from Vassar College and an MA in comparative literature from New York University, where she taught expository writing. Before coming to the MLA, she worked as an editor at a university press and as a freelance copyeditor and translator for commercial and academic publishers.</p>
 </div>
 </div>
