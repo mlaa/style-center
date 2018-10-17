9b8895a9eb24c07640796cdf3069f565ec4fb2d7
tlewek <tlewek@mla.org>
2018-10-17 12:46:04 -0400

    Trac 10070 new author badge

---
+++ templates/authors/duffy.php
@@ -0,0 +1,6 @@
+<div class="instructor-intro tile instructor-tile instructor-tile-small">
+<div class="tile-link tile-body">
+		<div class="author-photo author-photo-guest"></div>
+		<p><strong>Caitlin Duffy</strong> is a former New York City secondary school teacher and a current PhD student in English literature at Stony Brook University, State University of New York.</p>
+</div>
+</div>
