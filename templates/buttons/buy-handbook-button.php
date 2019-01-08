d1dfdadddad6319836a9349a7f461f05ae11d9bf
Kevin Romoser <kevinromoser@gmail.com>
2019-01-08 17:17:47 -0500

    Change button text, add handbook image.

--- templates/buttons/buy-handbook-button.php
+++ templates/buttons/buy-handbook-button.php
@@ -1,14 +1,19 @@
 
-<div class="button-group">
-	<div class="drop-down button--slim button--left__dark" role="button">Buy the e-book <span class="rotate-90 select-handle dashicons dashicons-leftright"></span>
-		<ul class="drop-down-sub-menu">
-			<li><a href="">Kindle</a></li>
-			<li><a href="">Apple</a></li>
-			<li><a href="">Kobo</a></li>
-			<li><a href="">Nook Store</a></li>
-		</ul>
+<div class="handbook-ad">
 
+	<img src="//chaucer.mlacommons.org/app/themes/style-center/assets/dist/../images/handbook-covers.png" class="handbook-ad--image">
+
+	<div class="button-group">
+		<div class="drop-down button--slim button--left__dark" role="button">Get the e-book <span class="rotate-90 select-handle dashicons dashicons-leftright"></span>
+			<ul class="drop-down-sub-menu">
+				<li><a href="">Kindle</a></li>
+				<li><a href="">Apple</a></li>
+				<li><a href="">Kobo</a></li>
+				<li><a href="">Nook Store</a></li>
+			</ul>
+
+		</div>
+		<a class="button--slim button--right__light" href="https://www.mla.org/Publications/Bookstore/Nonseries/MLA-Handbook-Eighth-Edition">Order a print copy</a>
 	</div>
-	<a class="button--slim button--right__light" href="https://www.mla.org/Publications/Bookstore/Nonseries/MLA-Handbook-Eighth-Edition">Order from the MLA bookstore</a>
-</div>
 
+</div>
