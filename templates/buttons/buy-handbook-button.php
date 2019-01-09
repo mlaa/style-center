daaf27f7e24672972631e8d670c0ce4561ff3cd0
Kevin Romoser <kevinromoser@gmail.com>
2019-01-09 10:53:27 -0500

    Add handbook tile to homepage.
    Fix bugs/ajust styles to accommodate tile layout

--- templates/buttons/buy-handbook-button.php
+++ templates/buttons/buy-handbook-button.php
@@ -1,15 +1,17 @@
 
 <div class="handbook-ad">
 
+	
 	<img src="//chaucer.mlacommons.org/app/themes/style-center/assets/dist/../images/handbook-covers.png" class="handbook-ad--image">
+	<h2 class="handbook-ad--title"><em>MLA Handbook</em>, eighth&nbsp;edition</h2>
 
 	<div class="button-group">
 		<div class="drop-down button--slim button--left__dark" role="button">Get the e-book <span class="rotate-90 select-handle dashicons dashicons-leftright"></span>
 			<ul class="drop-down-sub-menu">
-				<li><a href="">Kindle</a></li>
-				<li><a href="">Apple</a></li>
-				<li><a href="">Kobo</a></li>
-				<li><a href="">Nook Store</a></li>
+				<li><a href="https://www.amazon.com/Handbook-Modern-Language-Association-America-ebook/dp/B01KOZCHII/" target="_blank" >Kindle <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="https://itunes.apple.com/us/book/mla-handbook/id1145936789?mt=11" target="_blank" >Apple <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="https://www.kobo.com/us/en/ebook/mla-handbook" target="_blank">Kobo <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="https://www.barnesandnoble.com/w/mla-handbook-the-modern-language-association-of-america/1124337671?ean=9781603292641" target="_blank">Nook <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
 			</ul>
 
 		</div>
