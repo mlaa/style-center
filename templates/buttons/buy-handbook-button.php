bb21b9cea3d7e106e8a782913e5cc6d71e49847d
Kevin Romoser <kevinromoser@gmail.com>
2019-01-22 17:07:38 -0500

    Add tracking links to buttons.

--- templates/buttons/buy-handbook-button.php
+++ templates/buttons/buy-handbook-button.php
@@ -8,14 +8,14 @@
 	<div class="button-group">
 		<button class="button--slim button--left__dark" id="e-book-drop-down" role="button" aria-role="button" tabindex="0">Get the e-book <span class="rotate-90 select-handle dashicons dashicons-leftright"></span>
 			<ul class="menu-hide" id="drop-down-menu">
-				<li><a href="https://www.amazon.com/Handbook-Modern-Language-Association-America-ebook/dp/B01KOZCHII/" target="_blank" >Kindle <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
-				<li><a href="https://itunes.apple.com/us/book/mla-handbook/id1145936789?mt=11" target="_blank" >Apple <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
-				<li><a href="https://www.kobo.com/us/en/ebook/mla-handbook" target="_blank">Kobo <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
-				<li><a href="https://www.barnesandnoble.com/w/mla-handbook-the-modern-language-association-of-america/1124337671?ean=9781603292641" target="_blank">Nook <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="http://www.mla12.org/kindle" target="_blank" >Kindle <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="http://www.mla12.org/itunes" target="_blank" >Apple <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="http://www.mla12.org/kobo" target="_blank">Kobo <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
+				<li><a href="http://www.mla12.org/nook" target="_blank">Nook <span class="rotate-45 ext-link dashicons dashicons-arrow-up-alt"></span></a></li>
 			</ul>
 
 		</button>
-		<a class="button--slim button--right__light" href="https://www.mla.org/Publications/Bookstore/Nonseries/MLA-Handbook-Eighth-Edition">Order a print copy</a>
+		<a class="button--slim button--right__light" href="http://www.mla12.org/mla-bookstore">Order a print copy</a>
 	</div>
 
 	<script>
