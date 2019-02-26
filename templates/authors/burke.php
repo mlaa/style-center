ad3e1718f9e6b5578fe307c7b02d5df408451df5
Tom Lewek <tlewek@mla.org>
2019-02-26 09:28:54 -0500

    fix typo in Burke badge

--- templates/authors/burke.php
+++ templates/authors/burke.php
@@ -1,6 +1,6 @@
 <div class="instructor-intro tile instructor-tile instructor-tile-small">
 <div class="tile-link tile-body">
 		<div class="author-photo author-photo-guest"></div>
-		<p><strong>Mike Burke</strong> associate professor of English at St. Louis Community College, Meramec, has taught English at community colleges part-time since 2002 and full-time since 2007. He has also taught at the United States Military Academy and Southern Illinois University, Edwardsville. He is on the executive committee of the MLA forum on community colleges.</p>
+		<p><strong>Mike Burke</strong>, associate professor of English at St. Louis Community College, Meramec, has taught English at community colleges part-time since 2002 and full-time since 2007. He has also taught at the United States Military Academy and Southern Illinois University, Edwardsville. He is on the executive committee of the MLA forum on community colleges.</p>
 </div>
 </div>
