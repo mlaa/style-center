5d186426cb9742d168c5ba567a83d8476f934195
Kevin Romoser <kevinromoser@gmail.com>
2019-03-06 15:46:04 -0500

    Debug BTS

--- page-templates/landing-page.php
+++ page-templates/landing-page.php
@@ -5,7 +5,7 @@
  * @package MLAStyleCenter
  */
 
-namespace MLA\Commons\Theme\MLAStyleCenter;
+
 
 get_header();
 
@@ -37,6 +37,25 @@ get_header();
 	<div class="column--container">
 		<div class="column col-2-of-3">
 			<h3>Recent questions from Ask the MLA</h3>
+
+			<?php $args = array('cat' => 3);
+
+// The Query
+$the_query = new WP_Query( $args );
+
+// The Loop
+if ( $the_query->have_posts() ) {
+	echo '<ul>';
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+		echo '<li>' . get_the_title() . '</li>';
+	}
+	echo '</ul>';
+	/* Restore original Post Data */
+	wp_reset_postdata();
+} else {
+	// no posts found
+} ?>
 			
 			<ul class="question-list">
 				<li class="question-list--question">Six most recent questions</li>
