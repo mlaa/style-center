235606a196b0a6890291a2242b029aace311d93b
jbetancourt <jbetancourt@mla.org>
2019-03-07 11:25:45 -0500

    changed author tax to mla_author

--- templates/content-all.php
+++ templates/content-all.php
@@ -7,27 +7,7 @@
 
 namespace MLA\Commons\Theme\MLAStyleCenter;
 
-$post_author_meta_value_map = array(
-	'carr'      => 'Nora Carr',
-	'foasberg'  => 'Nancy Foasberg',
-	'gibson'    => 'Angela Gibson',
-	'wirth'     => 'Eric Wirth',
-	'woods'     => 'Livia Arndal Woods',
-	'kandel'    => 'Michael Kandel',
-	'latimer'   => 'Barney Latimer',
-	'rappaport' => 'Jennifer Rappaport',
-	'grooms'	=> 'Russell Grooms',
-	'suffern'	=> 'Erika Suffern',
-	'hoffman'	=> 'Joan M. Hoffman',
-	'mla'		=> 'Modern Language Association',
-	'carillo' => 'Ellen Carillo',
-	'yang' => 'Alice Yang',
-	'wallace' => 'Joseph Wallace',
-	'brookbank-christenberry' => 'Elizabeth Brookbank and H. Faye Christenberry',
-	'duffy' => 'Caitlin Duffy',
-	'smith' => 'Bradley Smith',
-	'burke' => 'Mike Burke',
-);
+$style_authors_bio_enabled = class_exists( 'STYLE_AUTHOR_BIOS' )
 
 ?>
 
@@ -49,14 +29,14 @@ $post_author_meta_value_map = array(
 			You are viewing all posts tagged <a href="/tag/<?php single_tag_title(); ?>" rel="tag"><?php single_tag_title(); ?></a>
 		</p>
 	<?php endif; ?>
-	<?php if ( isset( $_GET['post_author'] ) ) :
-        $author =  get_term_by( 'slug', sanitize_text_field($_GET['post_author']), 'author' );
+	<?php if ($style_authors_bio_enabled && isset( $_GET['post_author'] ) ) :
+        $author =  get_term_by( 'slug', sanitize_text_field($_GET['post_author']), 'mla_author' );
 		add_filter('pre_get_posts', function($query) use ($author) {
 			$query->set('post_type', 'post');
 			$query->set('posts_per_page', -1);
 			$query->set('tax_query', array(
 				array(
-					'taxonomy' => 'author', //or tag or custom taxonomy
+					'taxonomy' => 'mla_author', //or tag or custom taxonomy
 					'field' => 'slug',
 					'terms' => array($author->slug)
 				)
@@ -85,8 +65,6 @@ if ( have_posts() ) :
 		if ( 'post' === get_post_type() ) {
 			$custom_fields = get_post_custom();
 
-			$post_author = ( !empty( $author ) ) ? $author->slug : !empty(get_the_terms( get_the_ID(), 'author' ))?get_the_terms( get_the_ID(), 'author' )[0]:'';
-
 			$post_thumbnail_class = (
 				isset( $custom_fields['autocrop_featured_image'] ) &&
 				'false' === $custom_fields['autocrop_featured_image'][0]
@@ -99,9 +77,9 @@ if ( have_posts() ) :
 			if (
 				! is_category() &&
 				! is_search() &&
-				in_array( $post_category->slug, ['behind-the-style', 'teaching-resources'] )
+				in_array( $post_category->slug, array('behind-the-style', 'teaching-resources') )
 			) {
-				$authors = get_the_terms( get_the_ID(), 'author' );
+				$authors = get_the_terms( get_the_ID(), 'mla_author' );
 				if( count( $authors ) >= 1 ) {
 
 					$i = 0;
@@ -169,7 +147,7 @@ if ( have_posts() ) :
 	endif;
 
 	if (
-		( in_array( get_query_var('paged'), [1, 0] ) && is_category( 'behind-the-style' ) && $count < 4 && has_post_thumbnail() ) ||
+		( in_array( get_query_var('paged'), array(1, 0) ) && is_category( 'behind-the-style' ) && $count < 4 && has_post_thumbnail() ) ||
 		( ! is_category( 'behind-the-style' ) && has_post_thumbnail() )
 	) :
 	?>
